ed084ce6b17ae1d217928683f94b98b96dd8f9d0
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-11-25 02:28:05 +0000

    count letters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion Passed: ${actual.toString()} === ${expected.toString()}`);
+    }
+    else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual.toString()} !== ${expected.toString()}`);
+    }
+};
+
+const countLetters = function(sentence) {
+    let results = {};
+    for (var letter of sentence) {
+        if(results[letter]) {
+            results[letter]++;
+        }
+        else {
+            results[letter] = 1;
+        }
+    }
+    return results;
+}
+
+countLetters("lighthouse in the house");
+assertEqual(countLetters("house")["h"], 1);
