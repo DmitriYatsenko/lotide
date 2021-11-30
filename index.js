f8a2e3eb150a0b6297612dc71dc854987d2d8591
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-11-30 07:08:50 +0000

    fixed

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ module.exports = {
     middle: middle
 };
 */
-const _ = require('./index')
-undefined
-    > _
-{
-    head: [Function: head],
-    tail: [Function: tail],
-    middle: [Function: middle]
-}
-> .exit
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+    head: head,
+    tail: tail,
+    middle: middle
+};
