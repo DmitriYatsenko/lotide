d113cd757769473b9ee51914c204020b88d0fd3a
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2023-05-19 03:06:35 +0000

    fl@10

--- flatten.js
+++ flatten.js
@@ -1,10 +1,24 @@
 // const array = process.argv[2];
 // const fk = process.argv[3];
 
+// function splitArray(array) {
+//     return array.reduce((a, b) => {
+//         return a.concat(b);
+//     });
+// }
+
 function splitArray(array) {
-    return array.reduce((a, b) => {
-        return a.concat(b);
-    });
+    var ret = [];
+    for(var i = 0; i < array.length; i++) {
+        if(Array.isArray(array[i])) {
+            ret = ret.concat(splitArray(array[i]));
+        } else {
+            ret.push(array[i]);
+        }
+    }
+    return ret;
 }
 
 console.log(splitArray([[1, 2, 3], [4, 5], [6]]));
+console.log(splitArray([[1, 2, 3], [[4, 5]], [6]]));
+console.log(splitArray([["fk", [5, ["69"]]], [6]]));
